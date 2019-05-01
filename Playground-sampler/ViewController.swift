932cbf21e70d4bfc9f73aa113e23709f4ec9b9b6
ikawashima-cq <i.kawashima@caraquri.com>
2019-05-01 19:58:30 +0900

    Initial Commit

---
+++ Playground-sampler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Playground-sampler
+//
+//  Created by Iichiro Kawashima on 2019/05/01.
+//  Copyright © 2019 Iichiro Kawashima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
